02385063f5b68930c3eadaa0b3a4b3c44a34ea0b
yiweicao <yiwei.cao@britishcouncil.org>
2018-04-27 10:32:05 +0800

    init

---
+++ Demo1/Demo1Page.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Xamarin.Forms;
+using Demo1.ViewModels;
+namespace Demo1
+{
+    public partial class Demo1Page : ContentPage
+    {
+        public Demo1Page()
+        {
+            InitializeComponent();
+            this.BindingContext = new DemoViewModel();
+
+            this.EntryTel.Text = "zhangsan";
+            //this.EntryTel.BackgroundColor = Color.Accent;
+        }
+
+    }
+}
---
+++ Demo1/ViewModels/DemoViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Demo1.ViewModels
+{
+    public class DemoViewModel : INotifyPropertyChanged
+    {
+        private string _account;
+
+        public DemoViewModel()
+        {
+            
+        }
+
+       
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Account { get => _account; set { _account = value; OnPropertyChanged("Account"); } }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
+
